838761c595cf2bb964a2e90b0726f3e26de7672a
priskuss <149112709+priskuss@users.noreply.github.com>
2024-04-25 12:38:29 +0200

    added file for constants

---
+++ ConstantsStorage.cs
@@ -0,0 +1,17 @@
+class Constant
+{
+    public const string UserTemperature = "User temperature: {0}";
+    public const string YRTemperature = "YR temperature: {0}";
+    public const string UserHigher = "Your temperature is higher than YR temperature.";
+    public const string UserLower = "Your temperature is lower than YR temperature.";
+    public const string UserSame = "Your temperature is the same as YR temperature.";
+    public const string TemperatureDifference = "Temperature difference: {0} degrees.";
+    public const string LogFile = "log.json";
+    public const string Error = "Error: {0}, {1}";
+    public const string InvalidInput = "Invalid input. Please enter a valid temperature.";
+    public const string EnterTemperature = "Enter your temperature measurement: ";
+    public const string BaseUrl = "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=59.9333&lon=10.7166";
+    public const string UserAgent = "MyTestApp/0.1";
+    public const string AirTemperature = "air_temperature";
+    public const string Json = "application/json";
+}
