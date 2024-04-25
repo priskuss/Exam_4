bbb7d078d58830022fdeca1e98b84f76f9314275
priskuss <149112709+priskuss@users.noreply.github.com>
2024-04-25 13:40:45 +0300

    Refactored file
    Removed variables that never were used
    Got rid of magic strings
    Split code in more methods

--- Program.cs
+++ Program.cs
@@ -1,69 +1,80 @@
 using System;
-using System.Net.Http;
+using System.Collections.Generic;
+using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
-using System.Diagnostics;
-using System.IO;
 
 namespace WeatherLog
 {
     class Program
     {
-        private const string UserAgent = "MyTestApp/0.1";
-        private const string BaseUrl = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.10&lon=9.58";
-
         static async Task Main(string[] args)
         {
             var userTemperature = WeatherLog.UserData.GetUserTemperature();
             Console.Clear();
-            Console.WriteLine($"User temperature: {userTemperature}");
+            Console.WriteLine(Constant.UserTemperature, userTemperature);
 
             var YRData = await API.GetWeatherDataAsync();
             if (YRData.HasValue)
             {
-                Console.WriteLine($"Current temperature: {YRData.Value}");
+                Console.WriteLine(Constant.YRTemperature, YRData.Value);
+
+                var temperatureDifference = CalculateTemperatureDifference(userTemperature, YRData.Value);
+                Console.WriteLine(Constant.TemperatureDifference, temperatureDifference);
 
                 CompareTemperatures(userTemperature, YRData.Value);
 
-                var data = CreateDataObject(userTemperature, YRData.Value);
+                var data = CreateDataObject(userTemperature, YRData.Value, temperatureDifference);
 
-                var existingData = await File.ReadAllTextAsync("log.json");
-
-                var dataList = string.IsNullOrEmpty(existingData)
-                    ? new List<object>()
-                    : new List<object> { JsonSerializer.Deserialize<dynamic>(existingData) };
-
-                dataList.Add(data);
-
-                var json = JsonSerializer.Serialize(dataList);
-
-                await File.WriteAllTextAsync("log.json", json);
+                await AppendDataToFileAsync(Constant.LogFile, data);
             }
         }
+
         static void CompareTemperatures(double userTemperature, double YRData)
         {
             if (userTemperature > YRData)
             {
-                Console.WriteLine("Your temperature is higher than YR temperature.");
+                Console.WriteLine(Constant.UserHigher);
             }
             else if (userTemperature < YRData)
             {
-                Console.WriteLine("Your temperature is lower than YR temperature.");
+                Console.WriteLine(Constant.UserLower);
             }
             else
             {
-                Console.WriteLine("Your temperature is the same as YR temperature.");
+                Console.WriteLine(Constant.UserSame);
             }
         }
+        static double CalculateTemperatureDifference(double userTemperature, double YRData)
+        {
+            var difference = userTemperature - YRData;
+            return Math.Round(Math.Abs(difference), 1);
+        }
 
-        static object CreateDataObject(double userTemperature, double YRData)
+        static object CreateDataObject(double userTemperature, double YRData, double temperatureDifference)
         {
             return new
             {
                 Date = DateTime.Now,
                 UserTemperature = userTemperature,
-                YRData = YRData
+                YRData = YRData,
+                TemperatureDifference = temperatureDifference
             };
+        }
+
+        static async Task AppendDataToFileAsync(string fileName, object data)
+        {
+            var existingData = await File.ReadAllTextAsync(fileName);
+
+            var dataList = string.IsNullOrEmpty(existingData)
+                ? new List<object>()
+                : JsonSerializer.Deserialize<List<object>>(existingData);
+
+            dataList.Add(data);
+
+            var json = JsonSerializer.Serialize(dataList);
+
+            await File.WriteAllTextAsync(fileName, json);
         }
     }
 }
