2c5909d7b18938fcff08215973f932e15192df93
priskuss <149112709+priskuss@users.noreply.github.com>
2024-04-25 13:37:33 +0300

    Refactored DataFromTheUser.cs

--- DataFromTheUser.cs
+++ DataFromTheUser.cs
@@ -1,16 +1,23 @@
+using System;
+
 namespace WeatherLog
 {
     class UserData
     {
         public static double GetUserTemperature()
         {
-            Console.Write("Enter your temperature measurement: ");
-            var input = Console.ReadLine();
-            if (input == null)
+            double temperature;
+            while (true)
             {
-                throw new ArgumentNullException(nameof(input), "Input cannot be null.");
+                Console.Write(Constant.EnterTemperature);
+                var input = Console.ReadLine();
+                if (double.TryParse(input, out temperature))
+                {
+                    break;
+                }
+                Console.WriteLine(Constant.InvalidInput);
             }
-            return double.Parse(input);
+            return temperature;
         }
     }
 }
