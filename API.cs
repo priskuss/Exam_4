21bfb72364f173cd0d019c60947140b8488961f2
priskuss <149112709+priskuss@users.noreply.github.com>
2024-04-28 01:00:05 +0300

    Refactored file

--- API.cs
+++ API.cs
@@ -3,37 +3,34 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text.Json;
 
-namespace WeatherLog
+public class API
 {
-    class API
+    private const string BaseUrl = Constant.BaseUrl;
+    private const string UserAgent = Constant.UserAgent;
+
+    public static async Task<double?> GetWeatherDataAsync()
     {
-        private const string BaseUrl = Constant.BaseUrl;
-        private const string UserAgent = Constant.UserAgent;
-
-        public static async Task<double?> GetWeatherDataAsync()
+        using (HttpClient client = new HttpClient())
         {
-            using (HttpClient client = new HttpClient())
+            client.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
+            HttpResponseMessage responseMessage = await client.GetAsync(BaseUrl);
+
+            if (responseMessage.IsSuccessStatusCode)
             {
-                client.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
-                HttpResponseMessage responseMessage = await client.GetAsync(BaseUrl);
+                string response = await responseMessage.Content.ReadAsStringAsync();
+                JsonDocument data = JsonDocument.Parse(response);
+                JsonElement root = data.RootElement;
+                List<JsonElement> timeseries = root.GetProperty("properties").GetProperty("timeseries").EnumerateArray().ToList();
 
-                if (responseMessage.IsSuccessStatusCode)
-                {
-                    string response = await responseMessage.Content.ReadAsStringAsync();
-                    JsonDocument data = JsonDocument.Parse(response);
-                    JsonElement root = data.RootElement;
-                    List<JsonElement> timeseries = root.GetProperty("properties").GetProperty("timeseries").EnumerateArray().ToList();
+                JsonElement currentWeatherData = timeseries[0];
+                double temperature = currentWeatherData.GetProperty("data").GetProperty("instant").GetProperty("details").GetProperty("air_temperature").GetDouble();
 
-                    JsonElement currentWeatherData = timeseries[0];
-                    double temperature = currentWeatherData.GetProperty("data").GetProperty("instant").GetProperty("details").GetProperty("air_temperature").GetDouble();
-
-                    return temperature;
-                }
-                else
-                {
-                    string errorMessage = string.Format(Constant.Error, responseMessage.StatusCode, responseMessage.ReasonPhrase);
-                    throw new Exception(errorMessage);
-                }
+                return temperature;
+            }
+            else
+            {
+                string errorMessage = string.Format(Constant.Error, responseMessage.StatusCode, responseMessage.ReasonPhrase);
+                throw new Exception(errorMessage);
             }
         }
     }
